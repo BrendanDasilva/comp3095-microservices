9b5f9606888bbc4108011c8c60a66235cad6b8d9
Brendan Dasilva <brendan.dasilva@gmail.com>
2024-11-23 20:14:38 -0500

    got the OrderServiceApplicationTests working. I showed it erroring in the submission video but I have addressed the issue and have it operational

--- order-service/src/test/java/ca/gbc/orderservice/OrderServiceApplicationTests.java
+++ order-service/src/test/java/ca/gbc/orderservice/OrderServiceApplicationTests.java
@@ -1,6 +1,8 @@
 package ca.gbc.orderservice;
 
 import ca.gbc.orderservice.stub.InventoryClientStub;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,30 +11,34 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class OrderServiceApplicationTests {
+	@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+	@TestPropertySource(properties = "wiremock.server.port=8089")
+	class OrderServiceApplicationTests {
 
 
-	@ServiceConnection
-	static PostgreSQLContainer<?> postgresDB = new PostgreSQLContainer<>("postgres:latest");
+		@ServiceConnection
+		static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
 
-	@LocalServerPort
-	private Integer port;
+		@LocalServerPort
+		private Integer port;
 
-	@BeforeEach
-	void setup() {
-		RestAssured.baseURI = "http://localhost";
-		RestAssured.port = port;
-	}
+		@BeforeEach
+		void setUp() {
+			RestAssured.baseURI = "http://localhost/";
+			RestAssured.port = port;
+			WireMockServer wireMockServer = new WireMockServer(8089);
+			wireMockServer.start();
+			WireMock.configureFor("localhost", 8089);
+		}
 
-	static {
-		postgresDB.start();
-	}
+		static {
+			postgreSQLContainer.start();
+		}
 
 	@Test
 	void shouldSubmitOrder() {
