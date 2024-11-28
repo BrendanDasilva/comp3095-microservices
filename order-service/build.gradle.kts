3961e4b5a26da9266974b0d5da9eac7c00040b55
Brendan Dasilva <brendan.dasilva@gmail.com>
2024-11-28 09:46:03 -0500

    week 7.1 - kafka starting, concludes in next lesson

--- order-service/build.gradle.kts
+++ order-service/build.gradle.kts
@@ -42,6 +42,11 @@ dependencies {
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
 	implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j:3.1.2")
 	testImplementation("org.springdoc:springdoc-openapi-starter-webmvc-api:2.6.0")
+	// 7.1
+	implementation("org.springframework.kafka:spring-kafka:3.3.0")
+	testImplementation("org.springframework.kafka:spring-kafka-test:3.3.0")
+	testImplementation("org.testcontainers:kafka:1.20.4")
+	//
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
